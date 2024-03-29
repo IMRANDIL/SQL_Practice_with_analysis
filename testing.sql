cd758d2fa36f27344d7bc1953839629c25f11950
IMRANDIL <aliimranadil2@gmail.com>
2024-03-29 14:52:29 +0530

    created table for jan feb and march with jobs_date table with particular month data

--- testing.sql
+++ testing.sql
@@ -58,3 +58,35 @@
 -- ORDER BY
 -- count desc
 -- LIMIT 100;
+
+
+
+
+-- create 3 table ...1st table has only janury month data, 2nd table has february month data and 3rd table has march month data
+
+
+--january table..done
+-- CREATE TABLE january_jobs as
+--     SELECT 
+--     * 
+--     FROM job_postings_fact
+--     WHERE EXTRACT(MONTH FROM job_posted_date) = 1 
+--     AND
+--     salary_year_avg is not null
+
+
+
+-- feb table below
+
+-- CREATE TABLE feb_jobs as
+--     SELECT * FROM job_postings_fact
+--     WHERE EXTRACT(MONTH from job_posted_date) = 2
+--     and salary_year_avg is not null
+
+
+-- march job table below
+
+CREATE TABLE march_jobs as
+    SELECT * from job_postings_fact
+    WHERE EXTRACT(MONTH from job_posted_date) = 3
+    and salary_year_avg is not null
